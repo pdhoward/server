b1e150f78ae66c4ea7c2bdaf531a929aa78cc258
pdhoward <patrick.howard@hotmail.com>
2018-02-06 11:53:47 -0500

    updates

---
+++ db/data/agents.js
@@ -0,0 +1,71 @@
+
+///////////////////////////////////////////////////////////////////////
+///////////////////     placeholder for bots   ////////////////////
+//////////////////////////////////////////////////////////////////////
+const uuidv1 = require('uuid/v1');
+
+const objStore = [
+  {
+  avatarURL: "http://localhost:5002/avatar/persona/ade.jpg",
+	firstname: "Leila",
+  lastname: "Organa",
+  id: uuidv1(),
+  email: "leila@gmail.com",
+	cell: '704-555-1212',
+  subscribe:{
+    }
+  },
+  {
+  avatarURL: "http://localhost:5002/avatar/persona/justen.jpg",
+	firstname: "Hans",
+  lastname: "Solo",
+  id: uuidv1(),
+  email: "hans@gmail.com",
+	cell: '704-555-1212',
+  subscribe:{
+    }
+  },
+  {
+  avatarURL: "http://localhost:5002/avatar/bots/helpbot.jpg",
+	firstname: "Clone",
+  lastname: "Trooper",
+  id: uuidv1(),
+  email: "clone@gmail.com",
+	cell: '704-555-1212',
+  subscribe:{
+    }
+  },
+  {
+  avatarURL: "http://localhost:5002/avatar/persona/mark.png",
+	firstname: "Luke",
+  lastname: "Skywalker",
+  id: uuidv1(),
+  email: "luke@gmail.com",
+	cell: '704-555-1212',
+  subscribe:{
+    }
+  },
+  {
+  avatarURL: "http://localhost:5002/avatar/persona/joe.jpg",
+	firstname: "Lando",
+  lastname: "Calrissian",
+  id: uuidv1(),
+  email: "lando@gmail.com",
+	cell: '704-555-1212',
+  subscribe:{
+    }
+  },
+{
+  avatarURL: "http://localhost:5002/avatar/persona/steve.jpg",
+  firstname: "Anakin",
+  lastname: "Skywalker",
+  id: uuidv1(),
+  email: "anakin@gmail.com",
+  cell: '704-555-1212',
+  subscribe:{
+    }
+  }
+
+]
+
+module.exports = objStore;
